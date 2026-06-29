316476de859a5c9eed3200f590f2388c4de93831
Mural <apixymagic@gmail.com>
2026-06-30 04:22:30 +0530

    Move frontend install into build.js for Vercel compat

--- build.js
+++ build.js
@@ -1,2 +1,3 @@
 const { execSync } = require("child_process");
+execSync("npm install", { cwd: "frontend", stdio: "inherit" });
 execSync("node node_modules/vite/bin/vite.js build", { cwd: "frontend", stdio: "inherit" });
